d800926c73378afe1e44ea0f36aa73ff7692daef
Joel Hamilton <joelhamilton5@gmail.com>
2022-08-19 08:31:13 -0400

    tidy up

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script adds all nested git repos as submodules, and then initializes them
-
-add() {
-    echo $(cd "$1" && git config --get remote.origin.url)
-}
-
-find ./* -name .git | xargs add
